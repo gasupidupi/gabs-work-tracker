d6acb0a7eaf865524849893c6351a4f4b5269e61
gasupidupi <gasupidupi@gmail.com>
2025-08-24 12:34:26 +0200

    config command

--- tracker/main.py
+++ tracker/main.py
@@ -7,7 +7,7 @@ import importlib.resources
 
 app = typer.Typer()
 with importlib.resources.files("tracker").joinpath("config.yml").open("r") as f:
-    config = yaml.safe_load(f)
+    config_data = yaml.safe_load(f)
 
 
 @app.command()
@@ -22,19 +22,26 @@ def track(message: str = None, m: str = None, ticket: str = None, t: str = None)
         message = input("Enter message: ")
     if (ticket == None):
         ticket = input("Enter ticket: ")
-    with open(os.path.join(config["output_directory"], "tracks.csv"), "a") as f:
+    with open(os.path.join(config_data["output_directory"], "tracks.csv"), "a") as f:
         f.write(";".join([date, start, end, ticket, message]) + "\n")
     f.close()
 
 @app.command()
+def config(output: str = None, o: str = None):
+    output = output or o
+    config_data["output_directory"] = output
+    with importlib.resources.files("tracker").joinpath("config.yml").open("w") as f:
+        yaml.safe_dump(config_data, f)
+
+@app.command()
 def show():
-    with open(os.path.join(config["output_directory"], "tracks.csv"), "r") as f:
+    with open(os.path.join(config_data["output_directory"], "tracks.csv"), "r") as f:
         for line in f:
             print(line.replace(";", "   "))
             
 @app.command()
 def clear():
-    os.remove(os.path.join(config["output_directory"], "tracks.csv")) 
+    os.remove(os.path.join(config_data["output_directory"], "tracks.csv")) 
 
 
 def run() -> None:
